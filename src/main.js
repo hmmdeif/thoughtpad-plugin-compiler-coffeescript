4a3471ff9263cc96c1c410cdee886765389536c9
David Fisher-Moreau <deiform87@gmail.com>
2014-11-02 01:05:20 +0000

    Fix syntax (implement tests soon...)
    Signed-off-by: David Fisher-Moreau <deiform87@gmail.com>

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ var init = function (thoughtpad) {
     _thoughtpad.subscribe("javascript-compile-request", compileJs);
 },
 compileJs = function (contents) {
-    _thoughtpad.notify("javascript-compile-complete", coffee.compile(contents, {bare, true }));
+    _thoughtpad.notify("javascript-compile-complete", coffee.compile(contents, {bare: true }));
 };
 
 module.exports = {
