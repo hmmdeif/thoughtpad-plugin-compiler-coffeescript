e15acfcf236152be9746d9304e9df7bcefbbf83f
David Fisher-Moreau <deiform87@gmail.com>
2014-12-27 15:01:43 +0100

    Let plugin use config data
    Signed-off-by: David Fisher-Moreau <deiform87@gmail.com>

--- src/main.js
+++ src/main.js
@@ -7,7 +7,14 @@ var init = function (thoughtpad) {
 compileJs = function *(obj) {
     if (obj.ext !== "coffee") return;
 
-    yield obj.thoughtpad.notify("javascript-compile-complete", { contents: coffee.compile(obj.contents, {bare: true }), name: obj.name });
+    // The user can override this using the eventData config variable
+    var data = { bare: true };
+
+    if (obj.thoughtpad.config && obj.thoughtpad.config.eventData && obj.thoughtpad.config.eventData['javascript-compile']) {
+        data = obj.thoughtpad.config.eventData['javascript-compile'];
+    }
+
+    yield obj.thoughtpad.notify("javascript-compile-complete", { contents: coffee.compile(obj.contents, data), name: obj.name });
 };
 
 module.exports = {
